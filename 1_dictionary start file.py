ff2256bf144d8414940d7d0c5e3780dcf6ac4b96
Mary Hagan Chandler <mary_hagan1@baylor.edu>
2022-02-07 17:03:58 -0600

    Updated 1.0

--- 1_dictionary start file.py
+++ 1_dictionary start file.py
@@ -1,38 +1,31 @@
-phonebook = {'Chris':'555−1111',
-             'Katie':'555−2222',
-             'Joanne':'555−3333'}
-
-print()
-print('*****  start section 1 - print dictionary ********')
-print()
-
-
-
+phonebook = {"Chris": "555−1111", "Katie": "555−2222", "Joanne": "555−3333"}
 
 
 print()
-print('*****  end section 1 ********')
+print("*****  start section 1 - print dictionary ********")
+print()
+
+print(phonebook)
+
+print(len(phonebook))
+
+print(type(phonebook))
+
+mydictionary = dict(
+    m=8, n=9
+)  # Can be any numbers or any letters; this just creates a dictionary (because of the "dict()" function)
+print(mydictionary)
+
+chris_phone = phonebook["Chris"]
+print(chris_phone)
+
+print()
+print("*****  end section 1 ********")
 print()
 
 
+"""
 
-
-
-
-
-
-print()
-print('*****  start section 2 - search dictionary ********')
-print()
-
-
-
-
-
-
-print()
-print('*****  end section 2 ********')
-print()
 
 
 
@@ -41,32 +34,20 @@ print()
 
 
 print()
-print('*****  start section 3 - edit/append dictionary ********')
+print("*****  start section 2 - search dictionary ********")
 print()
 
 
+name = "chris"
 
-
-
-print()
-print('*****  end section 3 ********')
-print()
-
-
-
-
+if name in phonebook:
+    print(phonebook[name])
+else:
+    print("The name '" + name + "' is not found.")
 
 
 print()
-print('*****  start section 4 - delete/remove from dictionary ********')
-print()
-
-
-
-
-
-print()
-print('*****  end section 4 ********')
+print("*****  end section 2 ********")
 print()
 
 
@@ -74,16 +55,74 @@ print()
 
 
 
-print()
-print('*****  start section 5 - iterate through keys ********')
-print()
+
 
 
 
 
 print()
-print('*****  end section 5 ********')
+print("*****  start section 3 - edit/append dictionary ********")
 print()
+
+
+phonebook["Joe"] = "555-0123"
+phonebook["Chris"] = "555-4444"
+
+print(phonebook)
+
+print()
+print("*****  end section 3 ********")
+print()
+
+
+
+
+
+
+
+
+
+
+
+print()
+print("*****  start section 4 - delete/remove from dictionary ********")
+print()
+
+del phonebook["Chris"]
+
+print(phonebook)
+
+
+print()
+print("*****  end section 4 ********")
+print()
+
+
+
+
+
+
+
+
+
+print()
+print("*****  start section 5 - iterate through keys ********")
+print()
+
+for key in phonebook:
+    print(key)              #Iterates through the keys
+    print(phonebook[key])   #Iterates through the values    Remember that "key" is just a variable!
+
+
+print()
+print("*****  end section 5 ********")
+print()
+
+
+
+
+
+
 
 
 
@@ -93,6 +132,13 @@ print()
 print()
 print('*****  start section 6 - iterate through values  ********')
 print()
+
+
+
+for value in phonebook.values():     #values is the method of the phonebook object
+    #The word "value" is just a variable, but the method "values()" is a built-in method.
+    print(value)
+
 
 
 
@@ -108,12 +154,22 @@ print()
 
 
 
+
+
+
+
 print()
 print('*****  start section 7 - iterate through both key and value pair********')
 print()
 
+for phonebook_tuple in phonebook.items():
+    #The items() method allows us to get both the key and the value at the same time.
+    print(phonebook_tuple)
+    print(type(phonebook_tuple))
 
-
+for key, value in phonebook.items():
+    print(key)
+    print(value)
 
 
 print()
@@ -127,15 +183,26 @@ print()
 
 
 
-print()
-print('*****  start section 8 - using random and converting to list ********')
-print()
 
 
 
 
 print()
-print('*****  end section 8 ********')
+print("*****  start section 8 - using get and clear methods********")
+print()
+
+
+phone = phonebook.get("Chris", "key not found")
+
+print(phone)
+
+phonebook.clear()
+
+print(phonebook)
+
+
+print()
+print("*****  end section 8 ********")
 print()
 
 
@@ -145,3 +212,89 @@ print()
 
 
 
+
+
+
+print()
+print("*****  start section 9 - using pop and popitem methods********")
+print()
+
+# remove = phonebook.pop("Chris", "not found")
+
+# print(remove)
+
+# print(phonebook)
+
+
+
+
+
+a = phonebook.popitem()
+
+print(a)
+
+print(phonebook)
+
+
+print()
+print("*****  end section 9 ********")
+print()
+
+
+
+
+
+
+
+
+
+
+import random
+
+print()
+print("*****  start section 10 - using random and converting to list   ********")
+print()
+
+# This basically does what popitem should do.
+
+random_keys = list(phonebook)  #This creates a list from the dictionary. You need this to use random.choice.
+
+print(random_keys)
+
+choice = random.choice(random_keys)        ### random.choice is a function that only applies to lists
+
+print(choice)
+
+print(phonebook[choice])
+
+
+#TO CONDENSE THE ABOVE LINES, you can just do two lines of code:
+#choice = random.choice(list(phonebook))    
+#print(phonebook[choice])
+###OR
+#random_phone = phonebook[random.choice(list(phonebook))]
+#print(random_phone)
+
+print()
+print("*****  end section 10 ********")
+print()
+
+
+
+
+
+
+
+
+print()
+print('*****  start section 11 - using random and converting to list ********')
+print()
+
+
+
+
+print()
+print('*****  end section 11 ********')
+print()
+
+"""
--- 4_datastore.py
+++ 4_datastore.py
@@ -1,9 +1,9 @@
 # Below is a dictionary that contains information about real estate space for
 # a doctor's office. Using the dictionary, create a csv file that has details
-# for each space represented as rows. Name your file 'retail_space.csv.
+# for each space represented as rows. Name your file 'retail_space.csv.'
 
 
-'''
+"""
 Your final output should look like:
 
 room-number,use,sq-ft,price
@@ -13,37 +13,48 @@ room-number,use,sq-ft,price
 103,examination,125,150
 104,office,150,100
 
-'''
+"""
 
 
-
-
-datastore = { "medical":[
-      { "room-number": 100,
-        "use": "reception",
-        "sq-ft": 50,
-        "price": 75
-      },
-      { "room-number": 101,
-        "use": "waiting",
-        "sq-ft": 250,
-        "price": 75
-      },
-      { "room-number": 102,
-        "use": "examination",
-        "sq-ft": 125,
-        "price": 150
-      },
-      { "room-number": 103,
-        "use": "examination",
-        "sq-ft": 125,
-        "price": 150
-      },
-      { "room-number": 104,
-        "use": "office",
-        "sq-ft": 150,
-        "price": 100
-      }
-
-      ]
+datastore = {
+    "medical": [  # Datastore only has one key, medical. Medical has a list of keys, which are dictionaries.
+        {"room-number": 100, "use": "reception", "sq-ft": 50, "price": 75},
+        {"room-number": 101, "use": "waiting", "sq-ft": 250, "price": 75},
+        {"room-number": 102, "use": "examination", "sq-ft": 125, "price": 150},
+        {"room-number": 103, "use": "examination", "sq-ft": 125, "price": 150},
+        {"room-number": 104, "use": "office", "sq-ft": 150, "price": 100},
+    ]
 }
+
+# Answer is below
+outfile = open("retail_space.csv", "w")
+outfile.write("room-number,use,sq-ft,price\n")
+# Write a for loop here
+mylist = datastore[
+    "medical"
+]  # When you give a dictionary a key, it gives you the values. Since the value
+# of the key "medical" is a list of dictionaries, it gives us that.
+print(type(mylist))
+
+for l in mylist:  # l represents each dictionary in mylist
+    outfile.write(
+        str(
+            l["room-number"]
+        )  # You HAVE to convert to string before you write to a file.
+        + ","
+        + l[
+            "use"
+        ]  # you don't need a string function here because all of the uses are already strings
+        + ","
+        + str(l["sq-ft"])
+        + ","
+        + str(l["price"])
+        + "\n"
+    )
+# If something is a list, you need to give it an index value. If it's a dictionary, you need to give it a key.
+
+
+# for l in datastore["medical"]:
+#    print(type(l))  # This tells us that l is a dictionary
+
+outfile.close()
